a056793bb5255ff03ccc4044ce140d5084691c92
Frederick Legaspi <flegaspi700@yahoo.com>
2021-10-01 09:16:35 -0400

    Changes to SQL script

--- Azure/Data Engineer/Project07/SQL01-Serverless.sql
+++ Azure/Data Engineer/Project07/SQL01-Serverless.sql
@@ -12,12 +12,12 @@ CREATE MASTER KEY ENCRYPTION BY PASSWORD = '<password>';
 
 CREATE DATABASE SCOPED CREDENTIAL SasToken
 WITH IDENTITY='SHARED ACCESS SIGNATURE'
-, SECRET = '<SAS Token>';
+, SECRET = '<SasToken>';
 
 -- This defines the source of the data. 
 
 CREATE EXTERNAL DATA SOURCE log_data
-WITH (    LOCATION   = 'https://<source>.dfs.core.windows.net/data',
+WITH (    LOCATION   = 'https://<externalstoragename>.blob.core.windows.net/<container>',
           CREDENTIAL = SasToken
 )
 
@@ -33,29 +33,29 @@ CREATE EXTERNAL FILE FORMAT TextFileFormat WITH (
         FIELD_TERMINATOR = ',',
         FIRST_ROW = 2))
 
+-- If you made a mistake with the table, you can drop the table and recreate it again
+DROP EXTERNAL TABLE [logdata]
+
 -- Here we define the external table
 
 CREATE EXTERNAL TABLE [logdata]
 (
-    [Id] [int] NULL,
-	[Correlationid] [varchar](200) NULL,
-	[Operationname] [varchar](200) NULL,
-	[Status] [varchar](100) NULL,
-	[Eventcategory] [varchar](100) NULL,
-	[Level] [varchar](100) NULL,
-	[Time] [datetime] NULL,
-	[Subscription] [varchar](200) NULL,
-	[Eventinitiatedby] [varchar](1000) NULL,
-	[Resourcetype] [varchar](1000) NULL,
-	[Resourcegroup] [varchar](1000) NULL)
+    [Id] [int],
+	[Correlationid] [varchar](200),
+	[Operationname] [varchar](200),
+	[Status] [varchar](100),
+	[Eventcategory] [varchar](100),
+	[Level] [varchar](100),
+	[Time] [datetime],
+	[Subscription] [varchar](200),
+	[Eventinitiatedby] [varchar](1000),
+	[Resourcetype] [varchar](1000),
+	[Resourcegroup] [varchar](1000))
 WITH (
  LOCATION = '/Log.csv',
     DATA_SOURCE = log_data,  
     FILE_FORMAT = TextFileFormat
 )
-
--- If you made a mistake with the table, you can drop the table and recreate it again
-DROP EXTERNAL TABLE [logdata]
 
 /*
 Common errors
@@ -67,6 +67,7 @@ Here your Shared Access Siganture is an issue. Ensure to create the right Shared
 The maximum reject threshold is reached.
 This happens when you try to select the rows of data from the table. This can happen if the rows are not matching the schema defined for the table
 
+3. NULL is not allowed for external table columns.
 
 */
 
