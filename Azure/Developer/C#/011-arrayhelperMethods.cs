c8f5d29d8efe26cfcae547f0b7ca84d0ec6ec911
Frederick Legaspi <flegaspi700@yahoo.com>
2021-11-28 13:44:21 -0500

    New

--- Azure/Developer/C#/011-arrayhelperMethods.cs
+++ Azure/Developer/C#/011-arrayhelperMethods.cs
@@ -26,3 +26,103 @@ foreach (var pallet in pallets)
     Console.WriteLine($"-- {pallet}");
 }
 
+//The Array.Clear() method allows us to remove the contents of specific elements in our array.
+string[] pallets = { "B14", "A11", "B12", "A13" };
+console.WriteLine("");
+
+Array.Clear(pallets, 1, 2);
+console.WriteLine($"Cleared...: {pallets.Length}");
+foreach (var pallet in pallets)
+{
+    console.WriteLine($"-- {pallet}");
+}
+
+//The Array.Resize() method adds or removes elements from our array.
+string[] pallets = { "B14", "A11", "B12", "A13" };
+Console.WriteLine("");
+
+Array.Clear(pallets, 0, 2);
+Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+Array.Resize(ref pallets, 6);
+Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+pallets[4] = "C01";
+pallets[5] = "C02";
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+Array.Resize(ref pallets, 3);
+Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+//Reverse, then combine the char array into a new string
+string value = "abc123";
+char[] valueArray = value.ToCharArray();
+Array.Reverse(valueArray);
+string result = new string(valueArray);
+Console.WriteLine(result);
+// Output: 321cba
+
+//Combine all of the chars into a new comma-separated-value string using Join
+string value = "abc123";
+char[] valueArray = value.ToCharArray();
+Array.Reverse(valueArray);
+// string result = new string(valueArray);
+string result = String.Join(",", valueArray);
+Console.WriteLine(result);
+
+//split
+string value = "abc123";
+char[] valueArray = value.ToCharArray();
+Array.Reverse(valueArray);
+// string result = new string(valueArray);
+string result = String.Join(",", valueArray);
+Console.WriteLine(result);
+
+string[] items = result.Split(',');
+foreach (string item in items)
+{
+    Console.WriteLine(item);
+}
+
+//
+string pangram = "The quick brown fox jumps over the lazy dog";
+
+string[] message = pangram.Split(' ');
+string[] newMessage = new string[message.Length];
+
+for (int i = 0; i < message.Length; i++)
+{
+    char[] letters = message[i].ToCharArray();
+    Array.Reverse(letters);
+    newMessage[i] = new string(letters);
+}
+
+string result = String.Join(" ", newMessage);
+Console.WriteLine(result);
+
+//
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] items = orderStream.Split(',');
+
+foreach (var item in items)
+{
+    if (item.StartsWith("B"))
+    {
+        Console.WriteLine(item);
+    }
+}
