16df138f1455ceef7fb0a7662a1ceb16e2cba647
Frederick Legaspi <flegaspi700@yahoo.com>
2021-12-04 13:12:19 -0500

    Changes

--- Azure/Developer/dotNet/projects/001-DotNetDependencies/Program.cs
+++ Azure/Developer/dotNet/projects/001-DotNetDependencies/Program.cs
@@ -1,2 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/*
+<!-- Accepts any version 6.1 and later. -->
+<PackageReference Include="ExamplePackage" Version="6.1" />
+
+<!-- Accepts any 6.x.y version. -->
+<PackageReference Include="ExamplePackage" Version="6.*" />
+<PackageReference Include="ExamplePackage" Version="[6,7)" />
+
+<!-- Accepts any later version, but not including 4.1.3. Could be
+     used to guarantee a dependency with a specific bug fix. -->
+<PackageReference Include="ExamplePackage" Version="(4.1.3,)" />
+
+<!-- Accepts any version earlier than 5.x, which might be used to prevent pulling in a later
+     version of a dependency that changed its interface. However, we don't recommend this form because determining the earliest version can be difficult. -->
+<PackageReference Include="ExamplePackage" Version="(,5.0)" />
+
+<!-- Accepts any 1.x or 2.x version, but not 0.x or 3.x and later. -->
+<PackageReference Include="ExamplePackage" Version="[1,3)" />
+
+<!-- Accepts 1.3.2 up to 1.4.x, but not 1.5 and later. -->
+<PackageReference Include="ExamplePackage" Version="[1.3.2,1.5)" />
+*/
+using Humanizer;
+
+Console.WriteLine("Quantities:");
+HumanizeQuantities();
+
+Console.WriteLine("\nDate/Time Manipulation:");
+HumanizeDates();
+
+static void HumanizeQuantities()
+{
+    Console.WriteLine("case".ToQuantity(0));
+    Console.WriteLine("case".ToQuantity(1));
+    Console.WriteLine("case".ToQuantity(5));
+}
+
+static void HumanizeDates()
+{
+    Console.WriteLine(DateTime.UtcNow.AddHours(-24).Humanize());
+    Console.WriteLine(DateTime.UtcNow.AddHours(-2).Humanize());
+    Console.WriteLine(TimeSpan.FromDays(1).Humanize());
+    Console.WriteLine(TimeSpan.FromDays(16).Humanize());
+}
