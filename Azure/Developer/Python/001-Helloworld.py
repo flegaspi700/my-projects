1e5b9b68b9fe4676c48576c5032aa19c011f9e28
Frederick Legaspi <flegaspi700@yahoo.com>
2021-12-09 12:01:05 -0500

    Python Basics

--- Azure/Developer/Python/001-Helloworld.py
+++ Azure/Developer/Python/001-Helloworld.py
@@ -1,2 +1,40 @@
 #Hello World
 print("Hello World")
+
+#Printing a string
+color = "red"
+thing = "fire"
+print("The colors of the %s is %s" % (thing, color))
+
+print(color + " is the color of  " + thing)
+
+#Simple math
+print((((1+2)*3)/4)**5)
+#ratio
+ratio = ((1+(5**(1/2)))/2)
+print(ratio)
+
+#Arithmetic operators
+"""
+a + b = Adds a and b
+a - b = Subtracts b from a
+a * b = Multiplies a and b
+a / b = Divides a by b
+a ** b = Elevates a to the power of b. For non integer values of b, this becomes a root (i.e. a**(1/2) is the square root of a)
+a // b = The integer part of the integer division of a by b
+a % b = The remainder part of the integer division of a by b
+"""
+
+#calculate the sector of a disk
+disk_size = 16*1024**3
+sector_size = 512
+sectors = disk_size / sector_size
+print(sectors)
+
+#builtin functions
+#https://docs.python.org/3/library/functions.html
+
+#list of keywords in python
+#https://www.programiz.com/python-programming/keyword-list
+#https://docs.python.org/3/reference/lexical_analysis.html#keywords
+
---
+++ Azure/Developer/Python/005-Conditionals.py
@@ -0,0 +1,49 @@
+print(10>1)
+print(10<1)
+print(10==1)
+print(10!=1)
+print(10>=1)
+print(10<=1)
+print(10 is 1)
+print(10 is not 1)
+
+print("cat" == "Cat")
+
+print(1>2 and 2>3)
+print(1>2 or 2>3)
+print(not 1>2)
+
+# if elif else
+def hint_username(username):
+    if len(username) < 3:
+        return "Username must be 3 characters or more"
+    elif len(username) > 20:
+        return "Username must be 20 characters or less"
+    else:
+        return "Username is valid"
+
+print(hint_username("pe"))
+
+#
+def is_even(number):
+    if number % 2 == 0:
+        return True
+    return False
+
+print(is_even(3))
+
+#Calculate block size
+def calculate_block_size(file_size):
+    block_size = 4096
+    full_blocks = file_size // block_size
+    remaining_bytes = block_size % file_size
+
+    if remaining_bytes > 0:
+        return remaining_bytes * 2
+    else:
+        return block_size
+    
+print(calculate_block_size(1))    # Should be 4096
+print(calculate_block_size(4096)) # Should be 4096
+print(calculate_block_size(4097)) # Should be 8192
+print(calculate_block_size(6000)) # Should be 8192
