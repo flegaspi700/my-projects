21439952db64e4c8c33c1481ed2fe702591108b5
Frederick Legaspi <flegaspi700@yahoo.com>
2021-12-19 15:44:13 -0500

    Changes

--- Azure/Developer/Python/008-Strings.py
+++ Azure/Developer/Python/008-Strings.py
@@ -174,11 +174,10 @@ target_num_found = False
 for first_number in numbers:
     for second_number in numbers:
         print(first_number, second_number)
-        if first_number + second_number == target_number:
-            target_num_found = True
-            print(f"{first_number} + {second_number} = {target_number}")
+        if (first_number + second_number == target_number):
+                print(f"{first_number} + {second_number} = {target_number}")
 """
-
+"""
 def check_sum(numbers, target_number):
     for first_number in numbers:
         for second_number in numbers:
@@ -186,10 +185,18 @@ def check_sum(numbers, target_number):
                 message = f"{first_number} + {second_number} = {target_number}"
                 return message
     return False
-
+"""
 #print(check_sum(numbers, target_number))
 
 for i in range(len(numbers)):
     for j in range(len(numbers)):
-       if numbers.index(numbers[i]) != numbers.index(numbers[j]):
-           print(f"{numbers[i]} + {numbers[j]}")        
+       if (numbers.index(numbers[i]) != numbers.index(numbers[j])) and (numbers[i] + numbers[j] == target_number):
+           print(f"{numbers[i]} + {numbers[j]}") 
+
+for i in range(len(numbers)):
+    print(i)
+
+for number in numbers:
+    print(number, numbers.index(number))
+
+print(numbers.index(11))
